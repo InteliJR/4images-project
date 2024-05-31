0ca348364669992f81fddbfc9b975eeb9e0086ac
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-05-31 14:44:22 -0300

    🔨 add: Download simple MVC

---
+++ app/backend/4images/Controllers/DownloadController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _4images.Models;
+using _4images.Services;
+
+namespace _4images.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DownloadController : ControllerBase
+    {
+        private readonly DownloadService _downloadService;
+
+        public DownloadController(DownloadService downloadService)
+        {
+            _downloadService = downloadService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Download>>> GetDownloads()
+        {
+            var downloads = await _downloadService.GetAllDownloadsAsync();
+            return Ok(downloads);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Download>> GetDownload(int id)
+        {
+            var download = await _downloadService.GetDownloadByIdAsync(id);
+            if (download == null)
+            {
+                return NotFound();
+            }
+            return Ok(download);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Download>> CreateDownload(Download download)
+        {
+            var createdDownload = await _downloadService.CreateDownloadAsync(download);
+            return CreatedAtAction(nameof(GetDownload), new { id = createdDownload.Id }, createdDownload);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDownload(int id, Download download)
+        {
+            if (id != download.Id)
+            {
+                return BadRequest();
+            }
+
+            await _downloadService.UpdateDownloadAsync(download);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDownload(int id)
+        {
+            await _downloadService.DeleteDownloadAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ app/backend/4images/Models/Download.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _4images.Models
+{
+    public class Download
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int NumDownloads { get; set; }
+
+        [Required]
+        public int FileFK { get; set; }
+
+        // [ForeignKey("FileId")]
+        // public virtual File File { get; set; }
+
+        [Required]
+        public int TransactionFK { get; set; }
+
+        // [ForeignKey("TransactionId")]
+        // public virtual Transaction Transaction { get; set; }
+
+        [Required]
+        public int SignatureFK { get; set; }
+
+        // [ForeignKey("SubscriptionId")]
+        // public virtual Subscription Subscription { get; set; }
+    }
+}
