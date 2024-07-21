567dde695586f19429a436c759978542ea3f9b62
mauroDasChagas <mauro.junior@sou.inteli.edu.br>
2024-07-21 16:37:07 -0300

    :white_check_mark: feat: test: project test compatible with main project + basic user creation test for example

---
+++ app/backend/4images.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace _4images.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
